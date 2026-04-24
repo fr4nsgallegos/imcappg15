717070524f74b190a3988ede80bb057e40cd1384
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-04-23 22:56:29 -0500

    solucion con funciones

--- lib/pages/example_page.dart
+++ lib/pages/example_page.dart
@@ -1,40 +1,90 @@
 import 'package:flutter/material.dart';
 
 class ExamplePage extends StatefulWidget {
+  const ExamplePage({super.key});
+
   @override
   State<ExamplePage> createState() => _ExamplePageState();
 }
 
 class _ExamplePageState extends State<ExamplePage> {
   double textSize = 10;
+  double rojo = 0, verde = 0, azul = 0;
+
+  Widget _buildSlider(
+    String title,
+    double value,
+    Color color,
+    Function(double) onchanged,
+  ) {
+    return Column(
+      children: [
+        Text(title),
+        Slider(
+          min: 0,
+          max: 255,
+          value: value,
+          onChanged: onchanged,
+          activeColor: color,
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Style Editor")),
+      appBar: AppBar(title: Text("Slyder Editro")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+              "La noche está estrellada,y tiritan, azules, los astros, a lo lejos",
               style: TextStyle(
                 fontSize: textSize,
-                color: Color.fromRGBO(100, 250, 100, 1),
+                color: Color.fromRGBO(
+                  rojo.toInt(),
+                  verde.toInt(),
+                  azul.toInt(),
+                  1,
+                ),
               ),
             ),
-            Text("Editor de tamaño"),
             Slider(
               min: 5,
               max: 35,
+              activeColor: Colors.black,
               value: textSize,
-              onChanged: (double mandarina) {
-                textSize = mandarina;
+              onChanged: (double valor) {
+                textSize = valor;
                 setState(() {});
               },
             ),
-            // ACTIVIDAD
-            // Realizar 3 Sliders que manipulem los valores de R(red) G(green) B (blue), de modo que se modifique el color del texto a medida que voy alterando los sliders.
+            _buildSlider(
+              "Color Rojo",
+              rojo,
+              Colors.red,
+              (v) => setState(() {
+                rojo = v;
+              }),
+            ),
+            _buildSlider(
+              "Color verde",
+              verde,
+              Colors.green,
+              (v) => setState(() {
+                verde = v;
+              }),
+            ),
+            _buildSlider(
+              "Color azul",
+              azul,
+              Colors.blue,
+              (v) => setState(() {
+                azul = v;
+              }),
+            ),
           ],
         ),
       ),
