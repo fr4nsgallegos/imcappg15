0810b61ab97dd8db03aa5e917ba0c2c856058cc8
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-04-24 21:42:24 -0500

    avance1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:imcappg15/pages/example_page.dart';
+import 'package:imcappg15/pages/imc_page.dart';
 import 'package:imcappg15/pages/richtext_page.dart';
 import 'package:imcappg15/pages/slider_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: ExamplePage(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: ImcPage(), debugShowCheckedModeBanner: false));
 }
---
+++ lib/pages/imc_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class ImcPage extends StatefulWidget {
+  @override
+  State<ImcPage> createState() => _ImcPageState();
+}
+
+class _ImcPageState extends State<ImcPage> {
+  double altura = 0.5, peso = 40;
+
+  Widget _buildSlider({
+    required double value,
+    required bool isAltura,
+    required Function(double) onChanged,
+  }) {
+    return Column(
+      children: [
+        Text(isAltura ? "Altura" : "Peso"),
+        RichText(
+          text: TextSpan(
+            text: value.toStringAsFixed(1),
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            children: [
+              TextSpan(
+                text: " ${isAltura ? ' m' : ' kg'}.",
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+
+        Slider(
+          min: isAltura ? 0.4 : 40,
+          max: isAltura ? 2.1 : 130,
+          value: value,
+          onChanged: onChanged,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff4157B2),
+        foregroundColor: Colors.white,
+        title: Text("Calculadora IMC"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildSlider(
+              isAltura: true,
+              value: altura,
+              onChanged: (v) => setState(() {
+                altura = v;
+              }),
+            ),
+            _buildSlider(
+              value: peso,
+              isAltura: false,
+              onChanged: (v) => setState(() {
+                peso = v;
+              }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
