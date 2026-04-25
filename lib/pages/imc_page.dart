9c392dde7146a68a14d0d76bcd2d579e2f0e4c52
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-04-24 21:58:29 -0500

    calculo

--- lib/pages/imc_page.dart
+++ lib/pages/imc_page.dart
@@ -7,6 +7,12 @@ class ImcPage extends StatefulWidget {
 
 class _ImcPageState extends State<ImcPage> {
   double altura = 0.5, peso = 40;
+  double imcResult = 0;
+
+  double calcularIMC() {
+    double calculo = peso / (altura * altura);
+    return double.parse(calculo.toStringAsFixed(2));
+  }
 
   Widget _buildSlider({
     required double value,
@@ -69,6 +75,31 @@ class _ImcPageState extends State<ImcPage> {
               onChanged: (v) => setState(() {
                 peso = v;
               }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {
+                    imcResult = calcularIMC();
+                    setState(() {});
+                  },
+                  child: Text("Calcular"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff2E90F5),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              imcResult.toString(),
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
             ),
           ],
         ),
